48a228eba82f750dd750eac8765939c1d3bdefd8
SZIGETI János <szigeti@sch.bme.hu>
2014-12-29 22:52:55 +0100

    Aggregation supported (csvaggr).

---
+++ src/Numeric.cc
@@ -0,0 +1,95 @@
+/*
+ *  Copyright (C) 2014 SZIGETI János <szigeti at pilar dot hu>
+ *
+ *  This file is part of CsvTools.
+ *
+ *  CsvTools is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  CsvTools is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ * along with CsvTools. If not, see http://www.gnu.org/licenses/.
+ */
+#include "Numeric.h"
+
+using namespace std;
+
+int Numeric::digits_to_num(int a) {
+ int retv=1;
+ for (auto i=0;i<a;++i){retv*=10;}
+ return retv;
+}
+
+/// Standard constuctor as string parser.
+Numeric::Numeric(const string &a) {
+ val=0;
+ prec=0;
+ size_t idx;
+ bool negative=false;
+ size_t start=0;
+ try {
+  if (a.at(start)=='-'){
+   negative=true;
+   ++start;
+  }
+  if (a.at(start)=='.')
+   idx=start;
+  else {
+   val=stoi(a.substr(start),&idx);
+   idx+=start;
+  }
+  if (idx!=a.size()){
+   int xx=stoi(a.substr(idx+1),&prec);
+   val*=digits_to_num(prec);
+   val+=xx;
+  }
+  val*=(negative?-1:1);
+ } catch(...){
+ }
+}
+
+string Numeric::to_string() const {
+ auto str = std::to_string(val);
+ bool put_zero = (prec+(val<0?1:0) == str.size());
+ return prec?
+  (str.substr(0,str.size()-prec) +
+   (put_zero?"0":"") + "." +
+   str.substr(str.size()-prec)):
+  str;
+}
+
+Numeric &Numeric::operator+=(const string &a){
+ Numeric tmp(a);
+ while (prec<tmp.prec) {
+  val*=10;
+  ++prec;
+ }
+ while (tmp.prec<prec) {
+  tmp.val*=10;
+  ++tmp.prec;
+ }
+ val+=tmp.val;
+ return *this;
+}
+
+/// Very simple implementation of comparison.
+bool Numeric::operator<(const Numeric &x) const {
+ Numeric a(*this);
+ Numeric b(x);
+ while (a.prec<b.prec) {
+  a.val*=10;
+  ++a.prec;
+ }
+ while (b.prec<a.prec) {
+  b.val*=10;
+  ++b.prec;
+ }
+ return a.val<b.val; 
+};
+
---
+++ src/csvaggr.cc
@@ -0,0 +1,270 @@
+/*
+ *  Copyright (C) 2014 SZIGETI János <szigeti at pilar dot hu>
+ *
+ *  This file is part of CsvTools.
+ *
+ *  CsvTools is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  CsvTools is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ * along with CsvTools. If not, see http://www.gnu.org/licenses/.
+ */
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include "CsvPipe.h"
+#include "CsvRow.h"
+#include "Delimiters.h"
+#include "DefaultCommandLine.h"
+#include "Numeric.h"
+#include "log.h"
+#include "stl_out.h"
+
+using namespace std;
+
+const string DESCRIPTION="Aggregates columns of CSV file. Input file must be sorted.\n";
+const string USAGE="-a <name> <fun> <expr> [-a <name> <fun> <expr> [...]]\n"
+" name ::= str\t\t(name of the aggregated output column)\n"
+" expr ::= <ival>[,<expr>]\t\t(column expression)\n"
+" ival ::= num|-num|num-|num-num\t\t(column interval)\n"
+" fun ::= sum|min|max|count|concat\t\t(aggregation function, sum, min and max aggregates numeric values, count counts not empty cells, concat concatenates cells without separator)\n"
+"where str is string, num is integer\n";
+
+class AggrValue;
+
+struct AggrFun {
+ function<AggrValue &(AggrValue&, const string&)> initializer;
+ function<AggrValue &(AggrValue&, const string&)> accumulator;
+ function<string (const AggrValue&)> reader;
+ function<void (AggrValue&)> destructor;
+};
+
+/// Represents an aggregated value.
+/// Internally the aggregated value can be of ANY type.
+/// There are 3 requirements against the aggregated value:
+/// 1.) it must accept a string as initial value;
+/// 2.) it must accept a string value for accumulation;
+/// 3.) it must present a string value as result.
+class AggrValue {
+ AggrFun fun; 
+ bool has_dat;
+public:
+ void *dat;	///< Could not protect it because of lambdas.
+ AggrValue(const AggrFun &f):fun(f),has_dat(false){};
+ bool init(const string &a){if (!has_dat) {fun.initializer(*this,a); has_dat=true; return true;} return false;};
+ void accumulate(const string &a){fun.accumulator(*this,a);};
+ string get_dat(){return fun.reader(*this);};
+ bool cleanup(){if (has_dat) {fun.destructor(*this);has_dat=false; return true;} return false;};
+ bool valid_dat() const {return has_dat;};
+};
+
+typedef pair<string,AggrFun> AggrFunKey;
+typedef map<string,AggrFun> AggrFunMap;
+typedef tuple<char*, AggrFun, ColIvalV, FieldV> AggrCol;
+
+// Available column functions mapped by name.
+// Seems like templates could be used.
+const AggrFunMap colfun_m={
+ {"sum",{
+  [](AggrValue &a, const string &b)->AggrValue&{a.dat=new Numeric(b); return a;},
+  [](AggrValue &a, const string &b)->AggrValue&{*(reinterpret_cast<Numeric*>(a.dat))+=b; return a;},
+  [](const AggrValue &a)->string{return reinterpret_cast<Numeric*>(a.dat)->to_string();},
+  [](const AggrValue &a){delete reinterpret_cast<Numeric*>(a.dat);}}
+ },
+ {"min",{
+  [](AggrValue &a, const string &b)->AggrValue&{a.dat=new Numeric(b); return a;},
+  [](AggrValue &a, const string &b)->AggrValue&{
+   Numeric btmp(b);
+   Numeric *atmp=reinterpret_cast<Numeric*>(a.dat);
+   if (btmp<*atmp) *atmp=btmp;
+   return a;},
+  [](const AggrValue &a)->string{return reinterpret_cast<Numeric*>(a.dat)->to_string();},
+  [](const AggrValue &a){delete reinterpret_cast<Numeric*>(a.dat);}}
+ },
+ {"max",{
+  [](AggrValue &a, const string &b)->AggrValue&{a.dat=new Numeric(b); return a;},
+  [](AggrValue &a, const string &b)->AggrValue&{
+   Numeric btmp(b);
+   Numeric *atmp=reinterpret_cast<Numeric*>(a.dat);
+   if (*atmp<btmp) *atmp=btmp;
+   return a;},
+  [](const AggrValue &a)->string{return reinterpret_cast<Numeric*>(a.dat)->to_string();},
+  [](const AggrValue &a){delete reinterpret_cast<Numeric*>(a.dat);}}
+ },
+ {"count",{
+  [](AggrValue &a, const string &b)->AggrValue&{a.dat=new int(b.empty()?0:1); return a;},
+  [](AggrValue &a, const string &b)->AggrValue&{
+   if (!b.empty()) ++(*reinterpret_cast<int*>(a.dat));
+   return a;},
+  [](const AggrValue &a)->string{return to_string(*reinterpret_cast<int*>(a.dat));},
+  [](const AggrValue &a){delete reinterpret_cast<int*>(a.dat);}}
+ },
+ {"concat",{
+  [](AggrValue &a, const string &b)->AggrValue&{a.dat=new string(b); return a;},
+  [](AggrValue &a, const string &b)->AggrValue&{
+   (*reinterpret_cast<string*>(a.dat))+=b;
+   return a;},
+  [](const AggrValue &a)->string{return *reinterpret_cast<string*>(a.dat);},
+  [](const AggrValue &a){delete reinterpret_cast<string*>(a.dat);}}
+ }
+};
+
+// Aggregation specific options.
+const Option aggr_option_a[]={
+{"a","aggregation",3,APPEND,"Defines aggregation on column(s). arg1: new column name, arg2: aggr.function (sum|min|max|count|concat), arg3: column(s)"}
+};
+const int aggr_option_n = sizeof(aggr_option_a)/sizeof(Option);
+
+/// Extension to DefaultCommandLine: AggregationCommandLine can derive from option value(s) which columns are aggregated and which columns are grouped.
+class AggregationCommandLine : public DefaultCommandLine {
+ vector<AggrCol> aggr_v;
+ FieldV remain_v;	///< Set of fields that are not part of any aggregation.
+public:
+ AggregationCommandLine(const string desc, const string &usage) :
+  DefaultCommandLine(desc, usage,set<Option>(aggr_option_a,aggr_option_a+aggr_option_n)){};
+
+ int process() {
+  for (vector<char*> arg : get_values_for_flag("a")) {
+   char *name=arg[0];
+   char *fun=arg[1];
+   char *expr=arg[2];
+
+   ColIvalV col_v(expr);
+   AggrFunMap::const_iterator fmi=colfun_m.find(fun);
+   if (fmi==colfun_m.end()) {
+    ERROR(logger, "Unrecognized function ["<<fun<<"]. Aborting.");
+    return -2;
+   }
+   AggrFun af=fmi->second;
+   aggr_v.push_back(make_tuple(name,af,col_v,FieldV()));
+   INFO(global_logger, "Aggregation parsed.");
+  }
+  return 0;
+ };
+
+ void set_colnum(int size){
+  INFO(global_logger,"columns: "<<size);
+  auto all=ColIvalV("-").extract_ival(size);
+  set<ColID> remain_s(all.begin(),all.end());
+  for (AggrCol &aggr : aggr_v) {
+   get<3>(aggr) = get<2>(aggr).extract_ival(size);
+   for (auto field : get<3>(aggr)) {
+    remain_s.erase(field);
+   }
+  }
+  remain_v.clear();
+  remain_v.insert(remain_v.end(),remain_s.begin(),remain_s.end());
+  INFO(global_logger,"columns: "<<remain_s);
+ }
+ FieldV get_remain() const {return remain_v;};
+ CsvRow get_aggr_head() const {
+  CsvRow retv;
+  for_each(aggr_v.begin(),aggr_v.end(),
+   [&retv](const AggrCol &a){retv.push_back(CsvCell(get<0>(a),true));});
+  return retv;
+ };
+ vector<AggrCol> get_aggrcols() const {
+  return aggr_v;
+ };
+};
+
+
+void flush_aggr_line(
+ const CsvRow &ref,
+ ostream &os,
+ const Delimiters &delims,
+ const EscapeStrategy &strat,
+ vector<AggrValue> &avals,
+ bool refill=false,
+ vector<string> refill_v=vector<string>()
+){
+  ref.print(os,delims,strat);
+   if (!ref.empty() && !avals.empty()){
+    os<<delims.get(OFS);
+   }
+   for (unsigned int i=0; i < avals.size(); ++i) {
+    CsvCell acell(avals[i].get_dat(),false);
+    avals[i].cleanup();
+    acell.print(os,delims,strat);
+    if (i!=avals.size()-1){
+     os<<delims.get(OFS);
+    }
+    if (refill)
+     avals[i].init(refill_v[i]);
+   }
+   os<<delims.get(ORS);
+};
+
+
+int main(int argc, char **argv){
+ const Delimiters delims;
+ const EscapeStrategy strat=ESC_PRESERVE;
+
+ AggregationCommandLine cmdline=AggregationCommandLine(DESCRIPTION,USAGE);
+ CommandLineExecuteResponse resp=cmdline.execute(argc, argv);
+ if (resp!=CMDLINE_OK) {
+  return resp;
+ }
+// header
+ CsvRow head;
+ if (!head.parse(cin, delims)){
+  ERROR(cmdline.get_log_config(),"Cannot parse CSV header");
+  return -2;
+ }
+ cmdline.set_colnum(head.size());
+ (head.get_fields(cmdline.get_remain())+=cmdline.get_aggr_head())
+  .print(cout,delims,strat);
+ cout<<delims.get(ORS);
+
+// body
+ auto acols=cmdline.get_aggrcols();
+ vector<AggrValue> avalues;
+ for (auto acol : acols) {
+  avalues.push_back(AggrValue(get<1>(acol)));
+ }
+ 
+ bool first=true;
+ CsvRow ref;
+ CsvRow rx;
+ while (rx.parse(cin, delims)){
+  if (first) {
+   ref=rx.get_fields(cmdline.get_remain());
+   first=false;
+  }
+  CsvRow group=rx.get_fields(cmdline.get_remain());
+  vector<string> to_aggr;
+  for (auto acol : acols) {
+   stringstream ss;
+   rx.get_fields(get<3>(acol)).print(ss,Delimiters(),ESC_REMOVE);
+   to_aggr.push_back(ss.str());
+  }
+  if (group==ref) {
+   INFO(global_logger,"continuing aggregation.");
+// accumulate
+   for (unsigned int i=0; i < acols.size(); ++i) {
+    if (avalues[i].valid_dat()) {
+     avalues[i].accumulate(to_aggr[i]);
+    } else {
+     avalues[i].init(to_aggr[i]);
+    }
+   }
+  } else {
+   INFO(global_logger,"flushing aggregation.");
+// flush
+   flush_aggr_line(ref,cout,delims,strat,avalues,true,to_aggr);
+// set new ref
+   ref=group;
+  }
+  rx.clear();
+ }
+ flush_aggr_line(ref,cout,delims,strat,avalues);
+ return 0;
+}
+
