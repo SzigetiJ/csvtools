379d32caf7b9e831f40011025cb97b8b5091d03d
SZIGETI János <szigeti.janos@pilar.hu>
2021-05-06 11:15:00 +0200

    [ISSUE-14]: simple test

---
+++ tests/CsvCell_test.cc
@@ -0,0 +1,42 @@
+#include "CsvCell.h"
+#include <sstream>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+bool test_CsvCellPrintByStrat(const CsvCell &obj, EscapeStrategy strat, string expected) {
+  const Delimiters delims;
+  bool retv = true;
+
+  ostringstream ss;
+  obj.print(ss,delims,strat);
+  string actual = ss.str();
+
+  if (actual != expected) {
+    retv = false;
+    cerr<<"Diff. actual: ["<<actual<<"], expected: ["<<expected<<"]"<<endl;
+  }
+
+  return retv;
+}
+
+int main(int argc, const char *argv[]) {
+
+  assert(test_CsvCellPrintByStrat(CsvCell("hello",false), ESC_PRESERVE, "hello"));
+  assert(test_CsvCellPrintByStrat(CsvCell("hello",false), ESC_ALL, "\"hello\""));
+  assert(test_CsvCellPrintByStrat(CsvCell("hello",false), ESC_REMOVE, "hello"));
+  assert(test_CsvCellPrintByStrat(CsvCell("hello",false), ESC_RESOLVE, "hello"));
+
+  assert(test_CsvCellPrintByStrat(CsvCell("hello",true), ESC_PRESERVE, "\"hello\""));
+  assert(test_CsvCellPrintByStrat(CsvCell("hello",true), ESC_ALL, "\"hello\""));
+  assert(test_CsvCellPrintByStrat(CsvCell("hello",true), ESC_REMOVE, "hello"));
+  assert(test_CsvCellPrintByStrat(CsvCell("hello",true), ESC_RESOLVE, "\"hello\""));  // escaping was not requisite, still it was there. Not removing
+
+  assert(test_CsvCellPrintByStrat(CsvCell("quote\"within",true), ESC_PRESERVE, "\"quote\"\"within\""));
+  assert(test_CsvCellPrintByStrat(CsvCell("quote\"within",true), ESC_ALL, "\"quote\"\"within\""));
+  assert(test_CsvCellPrintByStrat(CsvCell("quote\"within",true), ESC_REMOVE, "quote\"within"));
+  assert(test_CsvCellPrintByStrat(CsvCell("quote\"within",true), ESC_RESOLVE, "\"quote\"\"within\""));
+
+  return 0;
+}
