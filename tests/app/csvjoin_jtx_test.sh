6392441dd8f764a9ee82b1ac8f105f3b20343a85
SZIGETI János <szigeti.janos@pilar.hu>
2022-02-05 21:26:12 +0100

    [ISSUE-14]: csvjoin test script

---
+++ tests/app/csvjoin_jtx_test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+WDIR="`dirname $0`"
+
+CSVJOIN="../../src/csvjoin"
+
+cat "$WDIR/t1.csv" | "$CSVJOIN" -jt natural -jf "$WDIR/t2.csv" | diff - "$WDIR/t1t2_natural.csv" || exit 1
+cat "$WDIR/t1.csv" | "$CSVJOIN" -jt inner -jc 1:0 -jf "$WDIR/t2.csv" | diff - "$WDIR/t1t2_natural.csv" || exit 1
+cat "$WDIR/t1.csv" | "$CSVJOIN" -jt outer -jc 1:0 -jf "$WDIR/t2.csv" | diff - "$WDIR/t1t2_outer.csv" || exit 1
+cat "$WDIR/t2.csv" | "$CSVJOIN" -jt cross -jf "$WDIR/t3.csv" | diff - "$WDIR/t2t3_cross.csv" || exit 1
