705146c714deee455021e2da697660cf178d8ba8
SZIGETI János <szigeti.janos@pilar.hu>
2022-02-05 17:31:12 +0100

    [ISSUE-14]: app tests (csvsort)

--- tests/app/csvproj_help_test.sh
+++ tests/app/csvproj_help_test.sh
@@ -5,4 +5,6 @@ WDIR="`dirname $0`"
 CSVPROJ="../../src/csvproj"
 
 cat "$WDIR/csvproj_help.txt.in" | sed "s:@@FULLPATH@@:$CSVPROJ:" > "./csvproj_help.txt"
-cat "$WDIR/t0.csv" | "$CSVPROJ" --help | diff - "./csvproj_help.txt"
+
+cat "$WDIR/t0.csv" | "$CSVPROJ" --help | diff - "./csvproj_help.txt" || exit 1
+cat "$WDIR/t0.csv" | "$CSVPROJ" -h | diff - "./csvproj_help.txt" || exit 1
