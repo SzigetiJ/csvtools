83c0b261514a0bf016d46adf6312fe303b977d79
SZIGETI János <szigeti.janos@pilar.hu>
2022-02-07 23:02:42 +0100

    [ISSUE-21]: app tests on errors

--- tests/app/csvproj_p1_test.sh
+++ tests/app/csvproj_p1_test.sh
@@ -5,3 +5,10 @@ WDIR=`dirname "$0"`
 CSVPROJ="../../src/csvproj"
 
 < "$WDIR/t0.csv" "$CSVPROJ" -c 1 | diff - "$WDIR/t0_proj1.csv"
+
+echo "## ERROR lines expected">&2
+< "$WDIR/t0.csv" "$CSVPROJ" -c >/dev/null && exit 1
+< "$WDIR/t0.csv" "$CSVPROJ" -c 1 0 >/dev/null && exit 1
+echo "## END (ERROR lines expected)">&2
+
+true
