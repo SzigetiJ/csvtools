0bcaeaff14cd9f5373d18bd24001c6a35745d75a
SZIGETI János <szigeti.janos@pilar.hu>
2022-02-08 21:18:18 +0100

    [ISSUE-14]: tests on numeric sort

--- tests/app/csvsort_k1x_test.sh
+++ tests/app/csvsort_k1x_test.sh
@@ -9,6 +9,7 @@ CSVSORT="../../src/csvsort"
 < "$WDIR/t0.csv" "$CSVSORT" -k 1d | diff - "$WDIR/t0_sort1d.csv" || exit 1
 < "$WDIR/t0.csv" "$CSVSORT" -k 1dn | diff - "$WDIR/t0_sort1dn.csv" || exit 1
 < "$WDIR/t1.csv" "$CSVSORT" -k 1e -k 0d | diff - "$WDIR/t1_sort1e0d.csv" || exit 1
+< "$WDIR/t5.csv" "$CSVSORT" -k 1n | diff - "$WDIR/t5_sort1n.csv" || exit 1
 
 echo "## ERROR lines expected">&2
 < "$WDIR/t1.csv" "$CSVSORT" -k >/dev/null && exit 1
